8348c6eb773e4b0096c4c9f8fa164082c12eed5c
fish <mikail.khan45@gmail.com>
2019-01-26 00:54:42 -0500

    template stuff

--- main/views.py
+++ main/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Article
 
 # Create your views here.
 
 def homepage(request):
-    return HttpResponse("AAAAAAAAAAAAAAAAAAAAAAA")
+    return render(request=request,
+                  template_name="main/home.html",
+                  context={"articles": Article.objects.all})
+         

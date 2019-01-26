61d44c0587950204f80dbadd16dac975a5e7fbea
fish <rayyan.khan258@gmail.com>
2019-01-25 22:01:34 -0500

    admin stuff

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Article(models.Model):
+    article_title = models.CharField(max_length=200)
+    article_content = models.TextField()
+    article_date = models.DateTimeField("Date Published", default=datetime.now())
+
+    def __str__(self):
+        return self.article_title
